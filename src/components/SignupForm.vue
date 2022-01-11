92fec3a87ba6837c3382c11932501ad8756191e1
VazMF <vazfernandam@gmail.com>
2022-01-11 09:00:30 -0300

    terminando

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -5,7 +5,7 @@
 
     <label>Password:</label>
     <input type="password" required v-model="password" />
-    <div v-if="passwordError"></div>
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
     <label>Role</label>
     <select v-model="role">
@@ -28,11 +28,6 @@
       <button>Create an Account</button>
     </div>
   </form>
-
-  <p>email: {{ email }}</p>
-  <p>password: {{ password }}</p>
-  <p>role: {{ role }}</p>
-  <p>terms: {{ terms }}</p>
 </template>
 
 <script>
@@ -64,7 +59,18 @@ export default {
     },
     handleSubmit() {
       // validate password
-      this.passwordError = this.password.length > 5 ? '' : 'Password must be at least 6 charts long'
+      this.passwordError =
+        this.password.length > 5
+          ? ""
+          : "Password must be at least 6 charts long";
+
+      if (!this.passwordError) {
+        console.log("email: ", this.email);
+        console.log("password:", this.password);
+        console.log("role: ", this.role);
+        console.log("skills: ", this.skills);
+        console.log("terms accepted: ", this.terms);
+      }
     },
   },
 };
@@ -128,5 +134,11 @@ button {
 }
 .submit {
   text-align: center;
+}
+.error {
+  color: #ff0062;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
 }
 </style>
